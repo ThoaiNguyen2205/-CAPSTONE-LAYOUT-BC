34c31ee79d92661e0ce76394d43bfd5eef20d9f8
ThoaiNguyen22 <thoainguyen22cn11@gmail.com>
2023-01-28 17:52:12 +0700

    dark-light

--- js/index.js
+++ js/index.js
@@ -14,3 +14,30 @@ $(function(){
        return window.pageYOffset || document.documentElement.scrollTop;
        }
    });
+// Simple dark mode with jQuery
+// Write by:‌ Mehdi Najafi
+
+$(function() {
+  $('#toggleDayOrNight').click(function(e) {
+      // toggle classes
+      $('#theme').toggleClass('header-dark-mode ')
+      $('.header-bottom').toggleClass('header-dark-mode')
+      $('.header-navbar a').toggleClass('navbar-dark')
+      $('.dropdown-menu').toggleClass('dark-theme')
+      $('.logo-text').toggleClass('dark-theme')
+      $('body').toggleClass('dark-theme')
+      // set background-image when clicked
+      if($('#theme')[0]) {
+        $('#toggleDayOrNight').css({'background-image':'url(https://img.icons8.com/dusk/64/000000/summer.png)',
+        'background-color': '#FFF'})
+        }
+         else {
+        $('#toggleDayOrNight').css({'background-image':'url(https://img.icons8.com/dusk/64/000000/new-moon.png)',
+        'background-color': '#000'})
+        }
+    
+  })
+})
+    
+
+  
